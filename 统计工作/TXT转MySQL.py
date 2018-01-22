ec506df7e564157d280b771874c89fe937385267
Heiybb <hf.heiybb@gmail.com>
2018-01-22 15:48:47 +0800

    TODO: SQL

--- 统计工作/TXT转MySQL.py
+++ 统计工作/TXT转MySQL.py
@@ -9,7 +9,10 @@ Con = open('MySQL_Converted.txt', 'w',encoding= 'utf8')
 for line in Pre.readlines():
     new_list = rpfun(line).strip('\n')
     if new_list != "":
-        Con.writelines("INSERT INTO `wuxia`.`reward` (`id`, `guild_num`,`rob`, `society_war`,`guild_act`,`guild_war`,`sum_re`,`real_re`,`date_end`)VALUES ('"+new_list+"');"+"\n")
+        if 'ID' in new_list:
+            pass
+        else:
+            Con.writelines("INSERT INTO `wuxia`.`reward` (`id`, `bangpai`,`weiren`, `zuixia`,`xuezhan`,`zhanchang`,`lueduo`,`zhengfeng`,`dkp`, `xiangzi`)VALUES ('"+new_list+"');"+"\n")
     else:
         pass
 Con.close()
